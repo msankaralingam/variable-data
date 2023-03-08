85e400f45c23debcfa126c29c1109a9346ee3ed1
Your Name <smile2sasi95@gmail.com>
2023-03-08 16:12:40 +0530

    add

---
+++ main.js
@@ -0,0 +1,90 @@
+document.write("variables&data&operators");
+console.log('variables&data&operators');
+
+var a=6;
+var b=9;
+var c=a+b;
+console.log(c);
+
+var a="6";
+var b=9;
+var c=a+b;
+console.log(c);
+
+
+let e=5;
+let f=7;
+let g=e*f;
+console.log(g);
+
+const i=5;
+const j=6;
+const k=i-j;
+console.log(k);
+
+ const l="7";
+ const m=`8`
+ console.log(typeof m);
+
+ 
+ const n=6;
+ const o=9;
+ console.log(typeof o);
+ 
+ 
+ const p=6;
+ const q=5;
+ var r=p>q;
+ console.log(typeof r);
+ 
+ let s
+ console.log(s);
+ 
+const t=null
+console.log(t);
+
+var a=2,b=3,c=4;
+ console.log((a<b)||(b<c)||(a<c));
+ 
+var a=10,b=5,c=20;
+console.log((a<b)&&(a++)<c);
+
+var a=7,b=5;
+console.log((a+b)&&(a-b)&&(a%b));
+
+ var a=1,b=2,c=3;
+console.log (!((a=b)&&(b===c)));
+
+
+var a="5";
+console.log(a);
+
+var a="10";
+var b=null;
+console.log(a,b);
+
+var x,y,z="5";
+console.log(x,y,z);
+
+
+var a=10;
+console.log(a);
+
+
+var x=10;
+x+=5;
+console.log(x);
+
+var y=15;
+y-=5;
+console.log(y);   
+
+var z=25;
+z/=5;
+console.log(z);
+
+
+
+
+
+
